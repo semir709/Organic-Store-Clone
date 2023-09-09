a0998fc887fbd3f360240138e55c09132abec730
semir709 <selmansemir555@gmail.com>
2023-09-09 20:33:39 +0200

    added the url root component

--- src/components/NavTrack.js
+++ src/components/NavTrack.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const NavTrack = () => {
+  const location = useLocation();
+
+  const string = location.pathname.slice(1).split("/");
+
+  let newString = string.map((el) => el.charAt(0).toUpperCase() + el.slice(1));
+
+  let text = "";
+
+  newString.forEach((el, index) => {
+    if (index === 0) text += el;
+    else text += " / " + el;
+  });
+
   return (
     <nav className="opacity-70 ">
-      <a href="/">Home</a> / Shop
+      <span className="text-lg">{text}</span>
     </nav>
   );
 };
